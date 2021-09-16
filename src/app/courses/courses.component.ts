6fad128f543b99e5574d5ca352e3c6f102888eec
Viswa Maheshwar Reddy <viswa.reddy@nexarc.in>
2021-09-16 18:36:24 +0530

    courses menu

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -9,6 +9,8 @@ import { ConnectionService } from '../connection.service';
 export class CoursesComponent implements OnInit {
 
   @Input() selectedCourse : string = '';
+  realestateCourses = REALESTATE_COURSES;
+  mortgageCourses = MORTGAGE_COURSES;
   constructor(private connectionService : ConnectionService) { }
 
   ngOnInit(): void {
@@ -18,3 +20,22 @@ export class CoursesComponent implements OnInit {
   }
 
 }
+export interface Courses {
+  courseName : string;
+  courseDesc : string;
+}
+
+const REALESTATE_COURSES : Courses[] = [
+  {courseName: 'REAL ESTATE ESSENTIALS', courseDesc: ''},
+  {courseName: 'RESIDENTIAL REAL ESTATE TRANSACTIONS', courseDesc: ''},
+  {courseName: 'ADDITIONAL RESIDENTIAL REAL ESTATE TRANSACTIONS', courseDesc: ''},
+  {courseName: 'SIMULATION FOR RESIDENTIAL REAL ESTAE TRANSACTION', courseDesc: ''},
+  {courseName: 'COMMERCIAL REAL ESTATE TRANSACTIONS', courseDesc: ''},
+  {courseName: 'SIMULATION FOR COMMERCIAL REAL ESTATE TRANSACTIONS', courseDesc: ''},
+  {courseName: 'BROKER QUALIFYING EXAM', courseDesc: ''},
+  {courseName: 'BROKER EXAM', courseDesc: ''},
+];
+const MORTGAGE_COURSES : Courses[] = [
+  {courseName: 'MORTGAGE EXAM', courseDesc: ''},
+];
+
