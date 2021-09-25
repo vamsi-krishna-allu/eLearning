61366169c0d9f95d29aee5f702d2580e95f77af1
Viswa Maheshwar Reddy <viswa.reddy@nexarc.in>
2021-09-25 12:26:17 +0530

    courses description

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { CoursepriceComponent } from './courseprice/courseprice.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CreditsComponent } from './credits/credits.component';
 import { FaqComponent } from './faq/faq.component';
 import { HomeComponent } from './home/home.component';
 import { MockTestsComponent } from './mock-tests/mock-tests.component';
 import { TestListComponent } from './mock-tests/test-list/test-list.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },  
@@ -19,7 +19,7 @@ const routes: Routes = [
   { path: 'contact', component: ContactsComponent }, 
   { path: 'credits', component: CreditsComponent},
   { path: 'mockTests', component: TestListComponent},
-  { path: 'subscribe', component: SubscribeComponent},
+  { path: 'subscribe', component: CoursepriceComponent},
   { path: '*', component: HomeComponent} 
 ];
 
