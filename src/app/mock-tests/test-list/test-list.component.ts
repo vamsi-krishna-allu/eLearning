cea4c72d0a1db1d25d4cb94cee95a2b69788047a
vamsikrish1 <vamsi010@outlook.com>
2021-10-21 21:10:38 +0530

    chnages done

--- src/app/mock-tests/test-list/test-list.component.ts
+++ src/app/mock-tests/test-list/test-list.component.ts
@@ -20,191 +20,173 @@ export class TestListComponent implements OnInit {
       courseId: 'real-estate-essentials',
       courseName: 'REAL ESTATE ESSENTIALS',
       isCourseAllowed: true,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'residential-real-estate-transactions',
       courseName: 'RESIDENTIAL REAL ESTATE TRANSACTIONS',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'additional-residential-real-estate-transactions',
       courseName: 'ADDITIONAL RESIDENTIAL REAL ESTATE TRANSACTIONS',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'simulation-residential-real-estate-transactions',
       courseName: 'SIMULATION FOR RESIDENTIAL REAL ESTAE TRANSACTION',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'commercial-real-estate-transactions',
       courseName: 'COMMERCIAL REAL ESTATE TRANSACTIONS',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'simulation-commercial-real-estate-transactions',
       courseName: 'SIMULATION FOR COMMERCIAL REAL ESTATE TRANSACTIONS',
       courseDesc: 'The prices of real-estate market influence the welfare of citizens and the business of real-estate investors. A well-known open challenge is to understand the repercussions of different combinations of individual buying/selling strategies on this market. The current approach is aimed at simulating these repercussions. For this purpose, a novel agent-based simulation tool includes common known strategies. This tool simulates the real-estate transactions from these strategies, showing the evolution of average prices and the results of each strategy (i.e. their success ratio, average price of their transactions and average waiting time). The underlying framework is extensible so that users can easily define and simulate new strategies. The experimentation of this work includes micro-validation of each kind of strategy',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'broker-qualifying-exam',
       courseName: 'BROKER QUALIFYING EXAM',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'broker-exam',
       courseName: 'BROKER EXAM',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
     {
       courseId: 'mortage-exam',
       courseName: 'MORTGAGE EXAM',
       isCourseAllowed: false,
-      test1:{
+      test:[{
         id: 'test-1',
         status: 'unavailable'
       },
-      test2:{
+      {
         id: 'test-2',
         status: 'unavailable'
-      },
-      test3:{
+      },{
         id: 'test-3',
         status: 'unavailable'
-      },
-      test4:{
+      },{
         id: 'test-4',
         status: 'unavailable'
-      }
+      }]
     },
   ];
 
@@ -215,27 +197,51 @@ export class TestListComponent implements OnInit {
     if(this.localStorageService.get('TOKEN')) {
       this.connectionService.getAvailableMockTests().subscribe((response: any) => {
         let availablemockTests = response.availableMockTests;
-        for(let mockTest of this.mockTests) {
-          let testIndex = availablemockTests.indexOf(mockTest.courseId);
-          if( testIndex !== -1) {
-            mockTest.isCourseAllowed = true;
+        let mockTestListCourseId = [];
+        if(availablemockTests && availablemockTests.length > 0){
+          for(let availableTest of availablemockTests) {
+            mockTestListCourseId.push(availableTest.substring(0, availableTest.length - 6));
+          }
+  
+          for(let mockTest of this.mockTests) {
+            let testIndex = mockTestListCourseId.indexOf(mockTest.courseId);
+            if( testIndex !== -1) {
+              let selectedTest = availablemockTests[testIndex];
+              let testNumber = selectedTest.substring(selectedTest.length-1);
+              mockTest.test[testNumber].status = 'available';
+            }
+          }
+        }
+        
+        let submitmockTests = response.submittedMockTests;
+        let submitTestListCourseId = [];
+
+        if(submitmockTests && submitmockTests.length > 0) {
+          for(let availableTest of submitmockTests) {
+            submitTestListCourseId.push(availableTest.substring(0, availableTest.length - 6));
+          }
+  
+          for(let mockTest of this.mockTests) {
+            let testIndex = submitTestListCourseId.indexOf(mockTest.courseId);
+            if( testIndex !== -1) {
+              let selectedTest = submitmockTests[testIndex];
+              let testNumber = selectedTest.substring(selectedTest.length-1);
+              mockTest.test[testNumber].status = 'submitted';
+            }
           }
         }
       });
     }
   }
 
-  subscribeNow(course: any) {
-    let planData = {courseName: course, type: 'course', planDetails: [{planType : 'basic', color:'red',   planPrice  : '$100', features : [`Any 1 mock test of ${course} available`,'Validity of the test - 30 days']},
+  subscribeNow(course: any, courseid: any) {
+    let planData = {courseId: courseid, courseName: course, type: 'course', planDetails: [{planType : 'basic', color:'red',   planPrice  : '$100', features : [`Any 1 mock test of ${course} available`,'Validity of the test - 30 days']},
                         {planType : 'standard', color: 'green', planPrice  : '$150', features : [`Any 2 mock tests of ${course} available`,'Validity of the test - 45 days']},
                         {planType : 'premium', color: 'blue', planPrice  : '$250', features : [`All 4 mock tests of ${course} available`,'Validity of the test - 60 days']}               
     ]};
     this.route.navigateByUrl('/subscribe', { state: { data: planData } });
   }
 
-  /*startTest(course: any) {
-    this.route.navigateByUrl('/openTest', {state: {data: `${course}-test-1`}});
-  }*/
   startTest(course: any) {
     this.testEnable = false;
     const dialogRef = this.dialog.open(TestInstructionsComponent, {
